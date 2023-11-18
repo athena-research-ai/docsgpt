370927178bab20ffbc41bb95c6261ddd84f542c4
pietrozullo <pietro.zullo@gmail.com>
2023-11-18 16:03:48 +0100

    File reader in utils

--- pypal/utils/file.py
+++ pypal/utils/file.py
@@ -7,6 +7,24 @@ Utilities to handle yaml, and json files.
 import yaml
 
 
+def read_file_as_string(file_path: str) -> str:
+    """Read file as a single string.
+
+    Parameters
+    ----------
+    file_path : str
+        Path of the file.
+
+    Returns
+    -------
+    str
+        Contents of the file as a string.
+    """
+    with open(file_path, "r") as file:
+        data = file.read()
+    return data
+
+
 def load_yaml_file(file_path):
     """
     Load the contents of a YAML file into a Python dictionary.
